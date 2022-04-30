758a2cec2ca1744dc4fe9dd9f5d2c420726681c2
xiaomeng7586 <758690263@qq.com>
2022-04-30 12:42:40 +0800

    feat:初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,18 @@
 module.exports = {
   root: true,
-
   env: {
-    node: true,
+    node: true
   },
-
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-  },
-
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/prettier",
-    "@vue/typescript",
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
   ],
-};
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <template>
-<div>vue3</div>
+  2333
+  <router-view/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<style lang="scss">
 
-export default defineComponent({
-  name: "App",
-  components: {
-  },
-});
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
