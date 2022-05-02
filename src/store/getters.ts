b61bc132b493db5df0f3f3127adedc302be01803
xiaomeng7586 <758690263@qq.com>
2022-05-02 15:49:48 +0800

    feat:头像菜单

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,6 +2,7 @@ const getters = {
   token: (state:any) => state.user.token,
   hasUserInfo: (state:any) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: (state:any) => state.user.userInfo
 }
 export default getters
