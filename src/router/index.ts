5d2acef76b477c816fa504518beefbeed71844d9
xiaomeng7586 <758690263@qq.com>
2022-05-02 18:21:06 +0800

    feat:创建路由表

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,103 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Layout from '@/layout/index.vue'
+
+const privateRoutes: Array<RouteRecordRaw> = [
+  {
+    path: '/user',
+    redirect: '/user/manage',
+    component: Layout,
+    meta: {
+      title: 'user',
+      icon: 'personnel'
+    },
+    children: [
+      {
+        path: '/user/manage',
+        name: 'userManage',
+        component: () => import('@/views/user-manage/index.vue'),
+        meta: {
+          title: 'userManage',
+          icon: 'personnel-manage'
+        }
+      },
+      {
+        path: '/user/role',
+        name: 'userRole',
+        component: () => import('@/views/role-list/index.vue'),
+        meta: {
+          title: 'userRole',
+          icon: 'role'
+        }
+      },
+      {
+        path: '/user/permission',
+        name: 'userPermission',
+        component: () => import('@/views/permission-list/index.vue'),
+        meta: {
+          title: 'userPermission',
+          icon: 'permission'
+        }
+      },
+      {
+        path: '/user/info/:id',
+        name: 'userInfo',
+        component: () => import('@/views/user-info/index.vue'),
+        meta: {
+          title: 'userInfo'
+        }
+      },
+      {
+        path: '/user/import',
+        name: 'import',
+        component: () => import('@/views/import/index.vue'),
+        meta: {
+          title: 'excelImport'
+        }
+      }
+    ]
+  },
+  {
+    path: '/article',
+    component: Layout,
+    redirect: '/article/ranking',
+    meta: {
+      title: 'article',
+      icon: 'article'
+    },
+    children: [
+      {
+        path: '/article/ranking',
+        component: () => import('@/views/article-ranking/index.vue'),
+        meta: {
+          title: 'articleRanking',
+          icon: 'article-ranking'
+        }
+      },
+      {
+        path: '/article/:id',
+        component: () => import('@/views/article-detail/index.vue'),
+        meta: {
+          title: 'articleDetail'
+        }
+      },
+      {
+        path: '/article/create',
+        component: () => import('@/views/article-create/index.vue'),
+        meta: {
+          title: 'articleCreate',
+          icon: 'article-create'
+        }
+      },
+      {
+        path: '/article/editor/:id',
+        component: () => import('@/views/article-create/index.vue'),
+        meta: {
+          title: 'articleEditor'
+        }
+      }
+    ]
+  }
+]
 
 const routePublic: Array<RouteRecordRaw> = [
   {
@@ -7,13 +106,38 @@ const routePublic: Array<RouteRecordRaw> = [
   },
   {
     path: '/',
-    component: () => import('@/layout/index.vue')
+    redirect: '/profile',
+    component: Layout,
+    children: [
+      // 个人中心
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index.vue'),
+        meta: {
+          title: 'profile',
+          icon: 'el-icon-user'
+        }
+      },
+      // 404
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404/index.vue')
+      },
+      // 401
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401/index.vue')
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: routePublic
+  routes: [...routePublic, ...privateRoutes]
 })
 
 export default router
