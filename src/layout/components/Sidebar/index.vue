933e09f912df1c386357765a30c68b0a2e411dad
xiaomeng7586 <758690263@qq.com>
2022-05-03 14:50:43 +0800

    feat:sidebarheader

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>占位符</h1>
+    <div class="logo-container">
+      <el-avatar size="44" shape="square" src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"></el-avatar>
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">mcc-admin</h1>
+    </div>
     <sidebar-menu></sidebar-menu>
   </div>
 </template>
@@ -9,6 +12,20 @@
 import SidebarMenu from './SidebarMenu.vue'
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
 </style>
