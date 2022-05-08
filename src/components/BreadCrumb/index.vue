6fc8e38bb9faa5322af37f351ba7b942ff203356
xiaomeng7586 <758690263@qq.com>
2022-05-08 12:32:34 +0800

    feat:根据数据动态修改面包屑

--- src/components/BreadCrumb/index.vue
+++ src/components/BreadCrumb/index.vue
@@ -1,22 +1,22 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item
-      ><a href="/">promotion management</a></el-breadcrumb-item
-    >
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <span class="no-redirect">promotion detail</span>
+
+    <el-breadcrumb-item v-for="(item,index) in breadcrumbData" :key="item.path">
+      <!-- 不可点击 -->
+      <span class="no-redirect" v-if="index === breadcrumbData.length - 1">{{item.meta.title}}</span>
+      <!-- 可点击 -->
+      <span class="redirect" v-else @click="onLink(item.path)">{{item.meta.title}}</span>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
 const route = useRoute()
-const breadcrumbData = ref<unknown>([])
+const breadcrumbData = ref<any>([])
 const formatBreadcrumbData = () => {
   breadcrumbData.value = route.matched.filter(item => item.meta && item.meta.title)
   console.log(breadcrumbData.value)
@@ -31,6 +31,16 @@ watch(
     immediate: true
   }
 )
+
+const router = useRouter()
+// 路由跳转
+const onLink = (path:string) => {
+  router.push(path)
+}
+
+// 主题替换颜色
+const store = useStore()
+const linkHoverColor = ref(store.getters.cssvar.menuBg)
 </script>
 
 <style lang="scss" scoped>
@@ -42,6 +52,14 @@ watch(
   ::v-deep .no-redirect {
     color: #97a8be;
     cursor: text;
+  }
+  ::v-deep .redirect {
+    cursor: pointer;
+    color: #666;
+    font-weight: 600;
+    &:hover {
+      color: v-bind(linkHoverColor);
+    }
   }
 }
 </style>
