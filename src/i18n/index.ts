8832c21f661d44f81847171fceb09a4d3a639a69
xiaomeng7586 <758690263@qq.com>
2022-05-14 17:16:25 +0800

    feat:国际化缓存的处理

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import enLocale from './lang/en'
 import zhLocale from './lang/zh'
+import store from '@/store'
 const messages = {
   en: {
     msg: {
@@ -14,14 +15,16 @@ const messages = {
   }
 }
 
-const locale = 'zh'
+const getLanguage = ():string => {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使用compositionAPI
   legacy: false,
   // 全局使用t函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
